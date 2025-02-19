0642bf0e53da14ba825324cb63668d5979e1eae7
KaiserinJacky <jacky.gaffney@gmail.com>
2025-02-19 00:29:48 -0500

    Update Roguelike.py
    small changes made while eepy

--- Roguelike.py
+++ Roguelike.py
@@ -10,49 +10,67 @@ class Room: # room class
 
 current_room = Room("No Monsters", "No Loot") # initial empty room
 
+class Spell: # spell class
+    def __init__(self, name, defense, targets, damage, mana_cost, partial, full, bonus):
+        self.name = name
+        self.defense = defense # defense the spell targets
+        self.targets = targets # who the spell targets
+        self.damage = damage # damage the spell does
+        self.mana_cost = mana_cost # mana cost of the spell
+        self.partial = partial # partial effect of spell
+        self.full = full # full effect of spell
+        self.bonus = bonus # bonus effect of spell on a critical
+
+    def __str__(self):
+        return self.name
+
+electric_field = Spell("Electric Field", "rc", "all", '1d6', 2,0,0,0)
+
+
     # gear + gear subclasses
 
 class Gear: # gear class
-    def __init__(self, kind):
+    def __init__(self, name, kind):
+        self.name = name
         self.kind = kind # type of gear - weapon, armor, focus, etc
+    def __str__(self):
+        return self.name
 
 class Weapon(Gear):
-    def __init__(self, damage_die, potency, traits):
-        super().__init__("Weapon")
+    def __init__(self, name, damage_die, potency, traits):
+        super().__init__(name, "Weapon")
         self.damage_die = damage_die
         self.potency = potency
         self.traits = traits
 
+arming_sword = Weapon("Arming Sword", '1d8',0, ["Sword"])
+steel_shield = Weapon("Steel Shield", '1d4', 0, ["Shield","Agile"])
+knife = Weapon("Knife", '1d4', 0, ["Knife","Agile","Finesse"])
+
 class Armor(Gear):
-    def __init__(self, ac_bonus, potency, traits):
-        super().__init__("Armor")
+    def __init__(self, name, ac_bonus, potency, traits):
+        super().__init__(name, "Armor")
         self.ac_bonus = ac_bonus
         self.potency = potency
         self.traits = traits
 
+chainmail = Armor("Chainmail", 3, 0, ["Plate"])
+wizard_robes = Armor("Wizard Robes", 0, 0, ["Robe"])
+
 class Focus(Gear):
-    def __init__(self, spells, potency, traits):
-        super().__init__("Focus")
+    def __init__(self, name, spells, potency, traits):
+        super().__init__(name, "Focus")
         self.spells = spells
         self.potency = potency
         self.traits = traits
 
-
+wand_electricity = Focus("Wand of Electricity", [electric_field], 0, ["Electricity"])
 
 class Artifact: # artifact class
     def __init__(self):
         # not sure what to put here yet. the artifacts are just a scrambled collection of special gear.
         # maybe shouldn't be its own class
         pass
-
-class Spell: # spell class
-    def __init__(self, defense, damage, mana_cost, partial, full, bonus):
-        self.defense = defense # defense the spell targets
-        self.damage = damage # damage the spell does
-        self.mana_cost = mana_cost # mana cost of the spell
-        self.partial = partial # partial effect of spell
-        self.full = full # full effect of spell
-        self.bonus = bonus # bonus effect of spell on a critical
 
     # creature and creature subclasses
 
@@ -64,7 +82,7 @@ class Creature: # Monsters and Players (And NPCs ??? - Later)
         self.per = per_base + level # perception
         self.speed = speed
         self.level = level
-        self.ac = 10 # add armor bonus based on gear?
+        self.ac = 10 + level # add armor bonus based on gear?
         self.maxhp = 8 + str * level # maximum hit points
         self.hp = 8 + str * level # current hit points - starts at max
         self.fc = 10 + str + level # fortitude class
@@ -80,8 +98,8 @@ class Creature: # Monsters and Players (And NPCs ??? - Later)
         self.satk = int + level # spell attack bonus
         self.gear = gear # list of gear held
 
-    def __str__(self):
-        return f"Str: {self.str} - Dex: {self.dex} - Int: {self.int}\nLevel: {self.level} - Per: {self.per} - Spd: {self.speed}\nHP: {self.hp} - SP: {self.sp} - MP: {self.mp}"
+    def __str__(self): # useful before we have any UI done
+        return f"Str: {self.str} - Dex: {self.dex} - Int: {self.int}\nLevel: {self.level} - Per: {self.per} - Spd: {self.speed}\nHP: {self.hp} - SP: {self.sp} - MP: {self.mp}\nFC: {self.fc} - RC: {self.rc} - WC: {self.wc} - AC: {self.ac}"
 
     def damage(self,damage):
         self.hp -= damage
@@ -126,9 +144,9 @@ class CharClass: # class for character classes
         self.starting_gear = starting_gear # starting gear for class
 
 # define base classes
-soldier = CharClass(3,1,1,2,20,("Basic Armor", "Basic Shield", "Basic Sword"))
-sorcerer = CharClass(1,1,3,2,25, ("Basic Wand", "Wizard Robes"))
-scoundrel = CharClass(1,3,1,3,30,"Basic Knife")
+soldier = CharClass(3,1,1,2,20,[chainmail, steel_shield, arming_sword])
+sorcerer = CharClass(1,1,3,2,25, ["Basic Wand", wizard_robes])
+scoundrel = CharClass(1,3,1,3,30,[knife])
 
 def check(bonus, dc):
     roll = dice.roll('1d20') + bonus
