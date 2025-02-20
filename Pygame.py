d33c7885dd76bc10e50a208862ef61aa4dafc2dd
KaiserinJacky <jacky.gaffney@gmail.com>
2025-02-20 10:23:43 -0500

    updated pygame.py
    added enum for areas, added scalex and scaley vars so we can size things in relation to screen size

--- Pygame.py
+++ Pygame.py
@@ -1,31 +1,28 @@
 # currently a hodgepodge between me messing around with pygame and me messing around with stuff for the actual game
 import pygame # pygame library
+from enum import Enum
+
+class Area(Enum):
+    MENU = 1
+    MAP = 2
+    COMBAT = 3
+
+current_area = Area.MENU
 
 # pygame stuff
 pygame.init()
 Screenwidth = 800
 Screenheight = 600
+scalex = Screenwidth / 100 # scale factors so that screen items aren't defined in pixel sizes
+scaley = Screenheight / 100
 # create screen
 screen = pygame.display.set_mode((Screenwidth, Screenheight))
 
-# dummy player
-player = pygame.Rect((300,250,50,50))
-
 game = True # bool that controls game
-while game:
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (255,0,0), player)
+while game: # main game loop
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a]:
-        player.move_ip(-1,0)
-    if key[pygame.K_d]:
-        player.move_ip(1, 0)
-    if key[pygame.K_s]:
-        player.move_ip(0, 1)
-    if key[pygame.K_w]:
-        player.move_ip(0, -1)
-    mouse_pos = pygame.mouse.get_pos()
+    if current_area == Area.MENU: # menu
+        screen.fill((0,0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # hit x button
@@ -33,4 +30,4 @@ while game:
 
     pygame.display.update()
 
-pygame.quit()
+pygame.quit() # close window when game over
